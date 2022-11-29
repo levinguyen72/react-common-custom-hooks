a0d3d4d23833acd9e5782dc1b614713d9fa88844
hoanv <hoahenry069@gmail.com>
2022-11-29 14:59:05 +0700

    feature: use query string

---
+++ src/hooks/useQueryString.js
@@ -0,0 +1,14 @@
+import { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+import qs from 'query-string';
+
+function useQueryString() {
+  const location = useLocation();
+  const queryString = useMemo(() => qs.parse(location.search), [location.search]);
+
+  return queryString; //  for example { page: 1, search: 'abc', filter: 'male' }
+}
+
+export default useQueryString;
+
+// common used for list like: pagination, sort, filter
